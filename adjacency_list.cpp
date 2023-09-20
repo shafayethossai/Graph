ece4130a8bb1fcc83fd449ff73f50e0326a208b1
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2023-09-20 21:31:50 +0600

    Update adjacency_list.cpp

--- adjacency_list.cpp
+++ adjacency_list.cpp
@@ -1,40 +1,62 @@
+// Representation of graph with Adjacency List
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,e;
-    cin>>n>>e;
+vector <int> graph[10000];
 
-    vector<int> graph[n];
+int main () {
+    int n, e;
+    cin >> n >> e;
 
-    for(int i=0; i<e; i++){
-        int u,v;
-        cin>>u>>v;
-
+    while (e--) {
+        int u, v;
+        cin >> u >> v;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
 
-    int q;
-    cin>>q;
+    for (int i = 1; i <= n; i++) { // highlight graph
+        cout << i;
 
-    for(int i=0; i<q; i++){
-        int u,v;
-        cin>>u>>v;
-        bool flag=false;
-        int len=graph[u].size();
+        for (int j = 0; j<graph[i].size(); j++) {
+            cout << "->" << graph[i][j];
+        }
+        cout << endl;
+    }
 
-        for(int j=0; j<len; j++){
-            if(graph[u][j]==v){
-                cout<<"YES"<<endl;
-                flag=true; break;
+    int q; // one node is connect to another node is yes or not
+    cin >> q;
+
+    for (int i = 0; i < q; i++) {
+        int x, y;
+        cin >> x >> y;
+        bool flag = false;
+        int len = graph[x].size();
+
+        for (int j = 0; j <= len; j++) {
+            if (graph[x][j] == y) {
+                cout << "Yes" << endl;
+                flag = true;
+                break;
             }
         }
-        if(!flag){
-            cout<<"NO"<<endl;
+        if (!flag) {
+            cout << "No" << endl;
         }
     }
 
+    int big = 0;
+    int index = 0;
+
+    for (int i = 1; i <= n; i++) { // finding max size with node
+        if (big < graph[i].size()) {
+            big = graph[i].size();
+            index = i;
+        }
+    }
+
+    cout << "node : " << index << " size : " << big << endl;
+
     return 0;
 }
-
