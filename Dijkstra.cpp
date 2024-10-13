be42b8c17fcd0be82e71ad802beafddcb6e401bd
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2024-10-14 00:36:17 +0600

    Add files via upload

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,11 +1,11 @@
-//The time complexity of Dijkstra's algorithm is O((V + E) * log(V)), where V is the number of vertices and E is the number of edges in the graph.
-
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
 #define INF 1000000000
 
 vector <pair<int,int>> graph[1001];
+vector <int> dist(1009, INF);
 
 int main () {
     int n, m, u, v, w;
@@ -17,8 +17,6 @@ int main () {
         graph[v].push_back(make_pair(u, w));
     }
     priority_queue <pair<int,int>, vector<pair<int,int> >, greater<pair<int,int> > >pq;
-
-    vector <int> dist(n+1, INF);
     pq.push(make_pair(0, 1));
     dist[1] = 0;
 
@@ -42,16 +40,3 @@ int main () {
 
     return 0;
 }
-
-/*
-6 9
-1 2 4
-1 6 2
-2 3 5
-2 6 1
-6 3 8
-6 5 10
-3 4 6
-3 5 3
-5 4 5
-*/
