daac589b7e89ea7bdfb72c0b003282cf5f0a3bfe
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2024-12-18 22:27:16 +0600

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -4,36 +4,42 @@ using namespace std;
 #define ll long long
 #define INF 1000000000
 
-vector <pair<int,int>> graph[1001];
-vector <int> dist(1009, INF);
+vector<pair<int, int>> graph[1001];
+vector<int> dist(1009, INF);
 
-int main () {
-    int n, m, u, v, w;
+int main() {
+    int n, m;
     cin >> n >> m;
 
     while (m--) {
+        int u, v, w;
         cin >> u >> v >> w;
-        graph[u].push_back(make_pair(v, w));
-        graph[v].push_back(make_pair(u, w));
+        graph[u].push_back({v, w});
+        graph[v].push_back({u, w});
     }
-    priority_queue <pair<int,int>, vector<pair<int,int> >, greater<pair<int,int> > >pq;
-    pq.push(make_pair(0, 1));
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    pq.push({0, 1});
     dist[1] = 0;
 
     while (!pq.empty()) {
-        int curr = pq.top().second;
+        int node = pq.top().second;
         int curr_d = pq.top().first;
         pq.pop();
 
-        for (auto child : graph[curr]) {
-            if (curr_d + child.second < dist[child.first]) {
-                dist[child.first] = curr_d + child.second;
-                pq.push(make_pair(dist[child.first], child.first));
+        for (auto child : graph[node]) {
+            int child_node = child.first;
+            int child_wt = child.second;
+
+            if (dist[child_node] > dist[node] + child_wt) {
+                dist[child_node] = dist[node] + child_wt;
+                pq.push({dist[child_node], child_node});
             }
         }
     }
 
-    for (int i=1; i<=n; i++) {
+    for (int i = 1; i <= n; i++) {
         cout << dist[i] << " ";
     }
     cout << endl;
