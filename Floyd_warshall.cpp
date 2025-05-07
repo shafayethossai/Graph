0f709c580aab81186ae9f68d091544b24ccabc14
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2025-05-07 14:24:49 +0600

    Update Floyd_warshall.cpp

--- Floyd_warshall.cpp
+++ Floyd_warshall.cpp
@@ -1,30 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int nodes, edges;
-    cin >> nodes >> edges;
-    int graph[nodes][nodes];
+#define ll long long
+const ll INF = 1e18;
 
-    for(int i=0;i<nodes;i++)
-        for(int j=0;j<nodes;j++)
-            graph[i][j] = (i==j)?0:99999;
+int main() {
+    ll n, m, q;
+    cin >> n >> m >> q;
+    ll graph[n+1][n+1];
 
-    for(int i=0;i<edges;i++){
-        int v1, v2, w;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            graph[i][j] = (i==j)?0:INF;
+        }
+    }
+    for(int i=0; i<m; i++){
+        ll v1, v2, w;
         cin >> v1 >> v2 >> w;
-        graph[v1][v2] = graph[v2][v1] = w;
+
+        graph[v1][v2] = min (graph[v1][v2], w);
+        graph[v2][v1] = min (graph[v2][v1], w);
+    }
+    for(int k = 1; k<=n; k++) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(graph[i][k] != INF && graph[k][j] != INF) {
+                    graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
+                }
+            }
+        }
     }
 
-    for(int k = 0;k<nodes;k++)
-        for(int i=0;i<nodes;i++)
-            for(int j=0;j<nodes;j++)
-                graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
-
+    while (q--) {
+        int u, v;
+        cin >> u >> v;
+        if(graph[u][v] >= INF) {
+            cout << "-1" << endl;
+        } 
+        else {
+            cout << graph[u][v] << endl;
+        }
+    }
     for(int i=0;i<nodes;i++){
-        for(int j=0;j<nodes;j++)
+        for(int j=0;j<nodes;j++) {
             cout << graph[i][j] << " ";
+        }
         cout << endl;
     }
+
     return 0;
 }
