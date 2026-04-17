1ab11dedf415f1cf5835407dce90018db28558f4
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2026-04-17 13:34:39 +0600

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,55 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int nodes, edges;
-    cin >> nodes >> edges;
-    int graph[nodes][nodes];
+#define INF 1000000000
+vector<pair<int, int>> graph[1001];
+vector<int> dist(1009, INF);
 
-    for(int i=0;i<nodes;i++)
-        for(int j=0;j<nodes;j++)
-            graph[i][j] = (i==j)?0:99999;
-
-    for(int i=0;i<edges;i++){
-        int v1, v2, w;
-        cin >> v1 >> v2 >> w;
-        graph[v1][v2] = graph[v2][v1] = w;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    while (m--) {
+        int u, v, w; 
+        cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+        graph[v].push_back({u, w});
     }
-
-    vector<int> d(nodes);
-    vector<int> visited(nodes);
-
-    for(int i=0;i<nodes;i++){
-        d[i] = 99999;
-        visited[i] = 0;
-    }
-
-    d[0] = 0;
-
-    while(1){
-        int min_d = 99999, min_n = -1;
-
-        for(int i=0;i<nodes;i++){
-            if(d[i]<99999 && !visited[i] && d[i] <= min_d ){
-                min_d = d[i];
-                min_n = i;
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, 1});
+    dist[1] = 0;
+    
+    while (!pq.empty()) {
+        int node = pq.top().second;
+        int curr_d = pq.top().first;
+        pq.pop();
+        
+        if (curr_d > dist[node]) continue;
+        
+        for (auto child : graph[node]) {
+            int child_node = child.first;
+            int child_wt = child.second;
+            
+            if (dist[child_node] > dist[node] + child_wt) {
+                dist[child_node] = dist[node] + child_wt;
+                pq.push({dist[child_node], child_node});
             }
         }
-
-        if(min_n == -1) break;
-        visited[min_n] = 1;
-
-        for(int j = 0;j<nodes;j++)
-            if( !visited[j] )
-                d[j] = min(d[j], d[min_n]+graph[min_n][j]);
-
     }
-
-    cout << "Shortest paths from 0: ";
-    for(int i=0;i<nodes;i++)
-        cout << d[i] << " ";
-
+    
+    for (int i = 1; i <= n; i++) {
+        cout << dist[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
