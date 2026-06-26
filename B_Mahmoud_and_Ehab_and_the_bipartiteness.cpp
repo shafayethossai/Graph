820cf59602e72110bd12f0dd7013dadfed877115
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2026-06-26 17:57:38 +0600

    Update B_Mahmoud_and_Ehab_and_the_bipartiteness.cpp

--- B_Mahmoud_and_Ehab_and_the_bipartiteness.cpp
+++ B_Mahmoud_and_Ehab_and_the_bipartiteness.cpp
@@ -7,18 +7,24 @@ using namespace std;
 
 const ll N = 1e5 + 9;
 vector<ll> g[N];
-vector<ll> vis(N, 0), col[2];
-
+vector<ll> vis(N, 0), col(2);
+ 
 void dfs(ll node, ll c) {
     col[c]++;
-
+    vis[node] = 1;
+ 
+    for (auto child : g[node]) {
+        if (!vis[child]) {
+            dfs(child, c^1);
+        }
+    }
 }
-
+ 
 void solve() {
     ll n, edge = 0;
     cin >> n;
     edge = n-1;
-
+ 
     while (edge--) {
         ll u, v;
         cin >> u >> v;
@@ -26,7 +32,7 @@ void solve() {
         g[v].push_back(u);
     }
     dfs(1, 0);
-
+ 
     cout << (col[0] * col[1]) - (n-1)<< endl;
 }
 
